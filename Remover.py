4226464d231c73adf0207d11f9368703d7a84fc8
David Lloyd <david.george.lloyd@gmail.com>
2015-03-28 10:11:15 -0700

    Update Remover.py

--- Remover.py
+++ Remover.py
@@ -1,21 +1,32 @@
+# import urllib.request to use password manager, http handler and opener
 import urllib.request
+
+# import json to use javascript object notation to parse data that has been converted to string
 import json
 
+# set variables to connect to uTorrent web UI
 url = "http://localhost:8080/gui/?list=1"
 port = 8080
 username = 'admin'
 password = 'admin'
 match = None
 
+# initialize password manager, http handler and opener
 password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
 password_mgr.add_password(None, url, username, password)
 handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
 opener = urllib.request.build_opener(handler)
 
+# open URL to get data
 request = opener.open(url)
+
+# convert data to string in 'utf-8' format so can be converted to json.
 string = request.read().decode('utf-8')
+
+# convert string to json so python can manipulate better
 json_obj = json.loads(string)['torrents']
 
+# for each torrent in the json object, check if it is finished and delete if so.
 for torrent in json_obj:
     finished_parse = str(torrent).find("100.0 %")
     if finished_parse != -1:
