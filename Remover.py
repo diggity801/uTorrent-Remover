498d378b17d5f985080c6e3afb57a8e52fcf604e
Irfan Charania <irfan.charania@gmail.com>
2015-12-22 20:37:28 -0800

    Use requests library
    cuts down code and makes it work with both python 2 and 3

--- Remover.py
+++ Remover.py
@@ -1,35 +1,26 @@
-# import urllib.request to use password manager, http handler and opener
-import urllib.request
-
-# import json to use javascript object notation to parse data that has been converted to string
-import json
+# import requests
+import requests
 
 # set variables to connect to uTorrent web UI
 port = 8080
-url = "http://localhost:" + str(port) + "/gui/?list=1"
 username = 'admin'
 password = 'admin'
-match = None
 
-# initialize password manager, http handler and opener
-password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-password_mgr.add_password(None, url, username, password)
-handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-opener = urllib.request.build_opener(handler)
+# set action urls
+list_url = "http://localhost:{port}/gui/?list=1".format(port=port)
+remove_url = "http://localhost:{port}/gui/?action=remove&hash={match}"
 
 # open URL to get data
-request = opener.open(url)
+r = requests.get(list_url, auth=(username, password))
 
-# convert data to string in 'utf-8' format so can be converted to json
-str_obj = request.read().decode('utf-8')
-
-# parse json string and return json object using 'torrents' index to narrow object to just torrents
-json_obj = json.loads(str_obj)['torrents']
+# return json object using 'torrents' index to narrow object to just torrents
+json_obj = r.json()['torrents']
 
 # for each torrent in the json object, check if it is finished and delete if so
 for torrent in json_obj:
     finished_parse = str(torrent).find("100.0 %")
     if finished_parse != -1:
         match = torrent[0].strip("'")
-    if match != None:
-        remove = opener.open("http://localhost:" + str(port) + "/gui/?action=remove&hash=" + match)
+        if match:
+            remove = requests.get(remove_url.format(port=port, match=match),
+                                  auth=(username, password))
